a10e64632ee62b73c469c8da8462971a3f8a1d4f
unknown <stunzenas.aironas@gmail.com>
2023-02-17 22:44:16 +0200

    Changes commit

--- app/Services/DriverExpenseService.php
+++ app/Services/DriverExpenseService.php
@@ -4,13 +4,70 @@ namespace App\Services;
   
 class DriverExpenseService
 {
+    public $addToSecondDriver;
+
+    public $driverExpenses;
+
     public function calculateDriverExpenses(array $drivers, array $expenses)
     {
-        $result = [];
 
-        // Write your logic here.
-        // ...
+        foreach($drivers as $key => $driver){
+            $this->driverExpenses[$key] = ['name' => $driver, 'expenses' => []];
+        }
+        
+        $this->addToSecondDriver = false;
+        foreach($expenses as $name => $price) {
+            foreach($drivers as $key => $driver) {
+                $centsAfterSplit = round($price/2 - floor($price/2), 3);
+                $this->driverExpenses[$key]['expenses'][$name] = round($price/2, 2);
+            }
+            if ($this->countDecimals($centsAfterSplit) > 2) {
+                if ($this->addToSecondDriver) {
+                    $this->driverExpenses[0]['expenses'][$name] -= 0.01;
+                } else {
+                    $this->driverExpenses[1]['expenses'][$name] -= 0.01;
+                }
+                $this->addToSecondDriver = !$this->addToSecondDriver;
+            }
+        }
+        
+        $result = [
+            'driverExpenses' => $this->driverExpenses,
+            'expenses' => $expenses,
+            'drivers' => $drivers,
+            'totals' => $this->calculateTotals($expenses),
+        ];
 
         return $result;
+
+    }
+
+    private function countDecimals($number) 
+    {
+        $count = 0;
+        $decimal = strpos($number, ".");
+        if ($decimal !== false) {
+            $count = strlen($number) - $decimal - 1;
+        }
+        return $count;
+    }
+
+    private function calculateTotals($baseExpenses) 
+    {
+        $totalExpenses = [];
+        $totalExpenses['Total'] = 0;
+        foreach($baseExpenses as $baseExpense) {
+            $totalExpenses['Total'] += $baseExpense;
+        }
+
+        foreach($this->driverExpenses as $driverExpense) {
+            $totalExpenses[$driverExpense['name']] = 0;
+            foreach($driverExpense['expenses'] as $expense) {
+                $totalExpenses[$driverExpense['name']] += $expense;
+            }
+        }
+    
+        
+        return $totalExpenses;
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,36 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Test Task</title>
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}" >
+</style>
     </head>
     <body>
         <div>
-            <pre>{{ print_r($data, true) }}</pre>
+            <pre>@dump($data)</pre>
+            <table>
+                <tr>
+                    <th>Expense/Driver</th>
+                    <th>Amount, $</th>
+                    @foreach ($data['drivers'] as $driver)
+                        <th>{{ $driver }}</th>
+                    @endforeach
+                </tr>
+                @foreach ($data['expenses'] as $name => $price)
+                <tr>
+                    <td>{{ $name }}</td>
+                    <td>{{ $price }}</td>
+                    @foreach ($data['driverExpenses'] as $singleDriverExpenses)
+                        <td>{{ number_format($singleDriverExpenses['expenses'][$name], 2) }}</td>
+                    @endforeach
+                </tr>
+                @endforeach
+                <tr class='totals'>
+                    <td>Total:</td>
+                    @foreach ($data['totals'] as $total)
+                        <td>{{ $total }}</td>
+                    @endforeach
+                </tr>
+            </table>
         </div>
     </body>
 </html>
